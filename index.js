884ead07c5f49ddfa31f71fce830650729f121a6
Christina Bohn <christinajbohn@gmail.com>
2022-01-06 22:00:19 -0800

    Added required packages to index.js

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Where are you located?',
+        name: 'location',
+      },
+      {
+        type: 'input',
+        message: 'Enter content for your bio:',
+        name: 'bio',
+      },
+      {
+        type: 'input',
+        message: 'What is your LinkedIn URL?',
+        name: 'linkedin'
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub URL?',
+        name: 'github'
+        }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
